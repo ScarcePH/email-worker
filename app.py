a0e458f013cdb3ab6a267272022f6996d38d0d61
M4rxDev <marionrosete1@gmail.com>
2026-02-11 13:37:22 +0800

    add validate payment template

--- app.py
+++ app.py
@@ -15,20 +15,23 @@ MAILTRAP_URL = "https://send.api.mailtrap.io/api/send"
 
 @app.route("/send-email", methods=["POST"])
 def send_email():
-    data = request.json
+    data = request.json or {}
+
+    email_type = data.get("type")
+
+    if email_type == "validate_payment":
+        template = os.environ["VALIDATE_TEMPLATE"]
+    else:
+        return jsonify({"error": "Unsupported email type"}), 400
+
+    template_variables = data.get("template_variables")
+
 
     payload = {
-        "from": {
-            "email": "orders@scarceph.com",
-            "name": "Scarceᴾᴴ"
-        },
-        "to": [
-            {"email": data["to"]}
-        ],
-        "subject": data["subject"],
-        "text": data["text"],
-        "html": data["html"],
-        "category": "transactional"
+        "from": {"email": "orders@scarceph.com"},
+        "to": [{"email": data.get("to")}],
+        "template_uuid": template,
+        "template_variables": template_variables,
     }
 
     r = requests.post(
