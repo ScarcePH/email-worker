c1cce9d54b7a24024afba713205808e876f81a85
M4rxDev <marionrosete1@gmail.com>
2026-05-13 18:57:22 +0800

    forgot password

--- app.py
+++ app.py
@@ -13,35 +13,7 @@ MAILTRAP_TOKEN = os.environ["MAILTRAP_TOKEN"]
 MAILTRAP_URL = "https://send.api.mailtrap.io/api/send"
 
 
-@app.route("/send-email", methods=["POST"])
-def send_email():
-    data = request.json or {}
-
-    email_type = data.get("type")
-
-    if email_type == "validate_payment":
-        template = os.environ["VALIDATE_TEMPLATE"]
-    elif email_type == 'approve_payment':
-        template = os.environ['APPROVE_TEMPLATE']
-    elif email_type == 'decline_payment':
-        template = os.environ['DECLINE_TEMPLATE']
-    elif email_type == 'shipment_update':
-        template = os.environ['SHIPMENT_UPDATE']
-    elif email_type == "admin_order_notification":
-        template = os.environ['ADMIN_ORDER_NOTIFICATION']
-    else:
-        return jsonify({"error": "Unsupported email type"}), 400
-
-    template_variables = data.get("template_variables")
-
-
-    payload = {
-        "from": {"email": "orders@scarceph.com"},
-        "to": [{"email": data.get("to")}],
-        "template_uuid": template,
-        "template_variables": template_variables,
-    }
-
+def _send_mailtrap_payload(payload):
     r = requests.post(
         MAILTRAP_URL,
         headers={
@@ -56,3 +28,56 @@ def send_email():
         return jsonify(r.json()), r.status_code
 
     return jsonify({"status": "sent"})
+
+
+@app.route("/send-email", methods=["POST"])
+def send_email():
+    data = request.json or {}
+
+    email_type = data.get("type")
+    template_variables = data.get("template_variables") or {}
+
+    if email_type == "validate_payment":
+        template = os.environ["VALIDATE_TEMPLATE"]
+    elif email_type == 'approve_payment':
+        template = os.environ['APPROVE_TEMPLATE']
+    elif email_type == 'decline_payment':
+        template = os.environ['DECLINE_TEMPLATE']
+    elif email_type == 'shipment_update':
+        template = os.environ['SHIPMENT_UPDATE']
+    elif email_type == "admin_order_notification":
+        template = os.environ['ADMIN_ORDER_NOTIFICATION']
+    elif email_type == "password_reset":
+        template = os.environ.get("PASSWORD_RESET_TEMPLATE")
+        if not template:
+            reset_url = template_variables.get("reset_url")
+            store_name = template_variables.get("store_name", "ScarcePH")
+            expires_in = template_variables.get("expires_in", "1 hour")
+
+            payload = {
+                "from": {"email": "orders@scarceph.com"},
+                "to": [{"email": data.get("to")}],
+                "subject": f"Reset your {store_name} password",
+                "text": (
+                    f"Use this link to reset your {store_name} password: {reset_url}\n\n"
+                    f"This link expires in {expires_in}. If you did not request this, ignore this email."
+                ),
+                "html": (
+                    f"<p>Use this link to reset your {store_name} password:</p>"
+                    f"<p><a href=\"{reset_url}\">Reset password</a></p>"
+                    f"<p>This link expires in {expires_in}. If you did not request this, ignore this email.</p>"
+                ),
+            }
+
+            return _send_mailtrap_payload(payload)
+    else:
+        return jsonify({"error": "Unsupported email type"}), 400
+
+    payload = {
+        "from": {"email": "orders@scarceph.com"},
+        "to": [{"email": data.get("to")}],
+        "template_uuid": template,
+        "template_variables": template_variables,
+    }
+
+    return _send_mailtrap_payload(payload)
